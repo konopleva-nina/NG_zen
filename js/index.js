9fe69008984053ee012e2ae4b76b1cb0b7b4dea5
Nina Konopleva <orangemise@gmail.com>
2024-08-22 00:26:46 +0300

    Refactored index js file

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import { data } from './data.js';
 import { Download } from './widgets/Download/Download.js';
+import { Warranty } from './widgets/Warranty/Warranty.js';
 
 const $root = document.querySelector('#root');
 
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
+$root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
 
 //Добавить в конспект оператор optional chaning
 // insertAdjasmentHtml
