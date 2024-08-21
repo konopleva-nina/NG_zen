161eaa2ea2a08bc663e89ae2274afc8d5d830014
Nina Konopleva <orangemise@gmail.com>
2024-08-21 22:47:05 +0300

    Refactored index js file

--- js/index.js
+++ js/index.js
@@ -1,20 +1,9 @@
-import { handleThemeBtnClick } from './handlers.js'
-import { handleConnectionNodeChange } from './handlers.js'
-import { handleMenuBurgerBtnClick } from './handlers.js'
+import { data } from './data.js';
+import { Download } from './widgets/Download/Download.js';
 
-//смена темы по кнопке луна/солнце
-const $app = document.querySelector('#app')
-const $themeBtn = document.querySelector('#theme')
+const $root = document.querySelector('#root');
 
-$themeBtn.addEventListener('click', handleThemeBtnClick)
-//стилизация спан элемента селекта
+$root?.insertAdjacentHTML('beforeend', Download(data.download));
 
-const $connectionNode = document.querySelector('#connection')
-
-document.addEventListener('change', handleConnectionNodeChange)
-//открытие/закрытие меню
-const $burgerBtn = document.querySelector('#burger')
-const $menu = document.querySelector('#menu')
-console.log($burgerBtn)
-
-$burgerBtn.addEventListener('click', handleMenuBurgerBtnClick)
+//Добавить в конспект оператор optional chaning
+// insertAdjasmentHtml
