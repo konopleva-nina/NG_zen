7960fd9cde325210f6b25290ec8bbde24db5bb7a
Nina Konopleva <orangemise@gmail.com>
2024-08-22 00:26:13 +0300

    Refactored data file

--- js/data.js
+++ js/data.js
@@ -61,7 +61,10 @@ export const data = {
       content: '1-year warranty boost',
       priority: 2,
     },
-    texts: ['ZEN cards have a very neat...', 'When you buy any electronics...'],
+    texts: [
+      'ZEN cards have a very neat feature that will help you whenever your electronic devices start failing after the standard warranty ends, which is pretty common.',
+      'When you buy any electronics with your ZEN shopping Mastercard, their warranty gets extended by one year. How does it work? It simply does. Automatically.',
+    ],
     image: {
       source: './assets/imag/basket.jpg',
       description: 'Basket with goods',
