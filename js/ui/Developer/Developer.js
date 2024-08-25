54ac4bf900ba78d9232366085c72c3afb28c634c
Nina Konopleva <orangemise@gmail.com>
2024-08-26 01:01:39 +0300

    Added Developer component

---
+++ js/ui/Developer/Developer.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {import('./types').DeveloperData} DeveloperData
+ */
+
+/**
+ * @function Developer
+ * @param {DeveloperData} data
+ * @returns {string}
+ */
+
+export const Developer = (data) => `
+  <div class="footer__dev">
+    <a class="footer__dev" href="${data.url}">
+      <img src="${data.content.data}" alt="developer" />
+    </a>
+  </div>
+`;
