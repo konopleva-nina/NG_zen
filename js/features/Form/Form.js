003a2e511c78126632b43d336b8561b979e15285
Nina Konopleva <orangemise@gmail.com>
2024-08-29 19:05:13 +0300

    Refactored Form component

--- js/features/Form/Form.js
+++ js/features/Form/Form.js
@@ -14,14 +14,19 @@
 export const Form = (input, select, selectText) => `
   ${input.map((inputItem) => `
     <label class="form__field">
-      <input type="${inputItem.type}" id="${inputItem.placeholder}" placeholder="${inputItem.placeholder}" />
+      <input type="${inputItem.type}"
+        id="${inputItem.placeholder}"
+        placeholder="${inputItem.placeholder}"
+      />
     </label>
   `).join('')}
   <label class="form__field">
     <span>${selectText}</span>
     <select id="connection">
       ${select.map((selectItem) => `
-        <option value="${selectItem.value}">${selectItem.optionText}</option>
+        <option value="${selectItem.value}">
+          ${selectItem.optionText}
+        </option>
       `).join('')}
     </select>
     <img src="/assets/icons/arrow.svg" alt="arrow select" />
