d0affd02dcaf0c5225767d263c90c8c1d53fd25d
Nina Konopleva <orangemise@gmail.com>
2024-09-08 19:53:38 +0300

    Added Select

---
+++ js/features/Select/Select.js
@@ -0,0 +1,26 @@
+/**
+ * @typedef {import('./types').SelectData} SelectData
+ */
+
+/**
+ * @function Select
+ * @param {SelectData[]} select
+ * @param {string} selectText
+ * @returns {string}
+ */
+
+export const Select = (select, selectText) => {
+  return `
+    <label class="form__field">
+      <span>${selectText}</span>
+      <select id="connection">
+        ${select.map((selectItem) => `
+          <option value="${selectItem.value}">
+            ${selectItem.optionText}
+          </option>
+        `).join('')}
+      </select>
+      <img src="/assets/icons/arrow.svg" alt="arrow select" />
+    </label>
+  `;
+};
