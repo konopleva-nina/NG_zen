db8c5208ad6e7852a84922dc3ec49c0537f2caa3
Nina Konopleva <orangemise@gmail.com>
2024-09-16 02:10:55 +0300

    Add data

---
+++ js/api/dataEn.js
@@ -0,0 +1,343 @@
+export const dataEn = {
+  primaryInfo: {
+    langs: [
+      {
+        key: 'en',
+        name: 'EN',
+      },
+      {
+        key: 'ru',
+        name: 'RU',
+      },
+    ],
+    navigation: [
+      {
+        key: 'download',
+        name: 'Download',
+      },
+      {
+        key: 'warranty',
+        name: 'Warranty',
+      },
+      {
+        key: 'care',
+        name: 'Care',
+      },
+      {
+        key: 'cashback',
+        name: 'Cashback',
+      },
+      {
+        key: 'clients',
+        name: 'Clients',
+      },
+    ],
+  },
+  download: {
+    title: {
+      content: 'Peaceful shopping mindful money',
+      priority: 1,
+    },
+    texts: [
+      'See how we can help with making your shopping experience and money management more ZEN.',
+    ],
+    links: [
+      {
+        type: 'image',
+        url: 'https://apple.com',
+        content: 'apple',
+      },
+      {
+        type: 'image',
+        url: 'https://google.com',
+        content: 'google',
+      },
+    ],
+    image: {
+      source: './assets/imag/bag_and_device.jpg',
+      description: 'Bag and device',
+    },
+  },
+  warranty: {
+    title: {
+      content: '1-year warranty boost',
+      priority: 2,
+    },
+    texts: [
+      'ZEN cards have a very neat feature that will help you whenever your electronic devices start failing after the standard warranty ends, which is pretty common.',
+      'When you buy any electronics with your ZEN shopping Mastercard, their warranty gets extended by one year. How does it work? It simply does. Automatically.',
+    ],
+    image: {
+      source: './assets/imag/basket.jpg',
+      description: 'Basket with goods',
+    },
+  },
+  care: {
+    title: {
+      content: 'care',
+      priority: 2,
+    },
+    texts: [
+      'Shopping online is usually a delightful experience. Until it\'s not.',
+      'Until something you buy, proves to be faulty, missing or a totally wrong item, you’re in for long product return procedures.',
+      'But, here comes ZEN Care: a free, built-in shopping protection.',
+    ],
+    image: {
+      source: './assets/imag/card.jpg',
+      description: 'Card',
+    },
+  },
+  cashback: {
+    title: {
+      content: 'and cashback!',
+      priority: 2,
+    },
+    texts: [
+      'It saves a little bit of money while you’re shopping, but in many cases it takes ages to get anything out of it.',
+      'We gave it a thought and made cashback way more friendly.',
+      'Up to 14% of what you spend using ZEN Mastercard ® will materialize on your account right after the purchase.',
+    ],
+    buttonText: 'Order a consultation!',
+  },
+  clients: {
+    brands: [
+      {
+        name: 'Rosegal',
+        logo: {
+          lightSource: './assets/brands/light/brand1.svg',
+          darkSource: './assets/brands/dark/brand1.svg',
+        },
+      },
+      {
+        name: 'Gearbest',
+        logo: {
+          lightSource: './assets/brands/light/brand2.svg',
+          darkSource: './assets/brands/dark/brand2.svg',
+        },
+      },
+      {
+        name: 'Allegro',
+        logo: {
+          lightSource: './assets/brands/light/brand3.svg',
+          darkSource: './assets/brands/dark/brand3.svg',
+        },
+      },
+      {
+        name: 'Aliexpress',
+        logo: {
+          lightSource: './assets/brands/light/brand4.svg',
+          darkSource: './assets/brands/dark/brand4.svg',
+        },
+      },
+      {
+        name: 'Booking',
+        logo: {
+          lightSource: './assets/brands/light/brand5.svg',
+          darkSource: './assets/brands/dark/brand5.svg',
+        },
+      },
+      {
+        name: 'Mediamarkt',
+        logo: {
+          lightSource: './assets/brands/light/brand6.svg',
+          darkSource: './assets/brands/dark/brand6.svg',
+        },
+      },
+      {
+        name: 'Rtveuroagd',
+        logo: {
+          lightSource: './assets/brands/light/brand7.svg',
+          darkSource: './assets/brands/dark/brand7.svg',
+        },
+      },
+      {
+        name: 'Groupon',
+        logo: {
+          lightSource: './assets/brands/light/brand8.svg',
+          darkSource: './assets/brands/dark/brand8.svg',
+        },
+      },
+    ],
+  },
+  secondaryInfo: {
+    columns: [
+      {
+        title: {
+          content: 'Lern',
+          priority: 3,
+        },
+        links: [
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Help center',
+          },
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Join ZEN team',
+          },
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Privacy Policy',
+          },
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Cookies',
+          },
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Terms of use',
+          },
+        ],
+      },
+      {
+        title: {
+          content: 'Discover',
+          priority: 3,
+        },
+        links: [
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'For developers',
+          },
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Apple Pay',
+          },
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Google Pay',
+          },
+        ],
+      },
+      {
+        title: {
+          content: 'Offer',
+          priority: 3,
+        },
+        links: [
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Business',
+          },
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Personal',
+          },
+        ],
+      },
+      {
+        title: {
+          content: 'Language',
+          priority: 3,
+        },
+        links: [
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'English',
+          },
+          {
+            url: 'https://example.com',
+            type: 'text',
+            content: 'Polish',
+          },
+        ],
+      },
+      {
+        title: {
+          content: 'Follow us',
+          priority: 3,
+        },
+        links: [
+          {
+            url: 'https://example.com',
+            type: 'image',
+            content: 'ln',
+          },
+        ],
+      },
+      {
+        title: {
+          content: 'Contact',
+          priority: 3,
+        },
+        links: [
+          {
+            url: 'mailto:hello@zen.com',
+            type: 'text',
+            content: 'hello@zen.com',
+          },
+        ],
+      },
+    ],
+    texts: [
+      'ZEN is a licensed financial institution under the supervision of the Central Bank of Lithuania approved by European Banking Authority for 31 countries.',
+      'Registration number of the company 304749651, VAT ID LT100011714916.',
+      'License of electronic money institutions number LB000457.',
+      'Share capital 2.185.000,00 EUR.',
+      'Company address: Mėsinių g. 5, LT-01133, Vilnius, Lithuania.',
+      'Sales Office: ul. Emilii Plater 53, 00-113 Warsaw, Poland.',
+    ],
+    developer: {
+      url: 'https://example.com',
+      content: {
+        type: 'image',
+        data: './assets/icons/developer.svg',
+      },
+    },
+  },
+  modal: {
+    title: {
+      content: 'Order a consultation',
+      priority: 3,
+    },
+    input: [
+      {
+        id: 'name',
+        type: 'text',
+        placeholder: 'Name',
+      },
+      {
+        id: 'tel',
+        type: 'tel',
+        placeholder: 'Tel.',
+      },
+      {
+        id: 'email',
+        type: 'email',
+        placeholder: 'E-mail',
+      },
+    ],
+
+    select: [
+      {
+        value: '',
+        optionText: '',
+      },
+      {
+        value: 'telegram',
+        optionText: 'Telegram',
+      },
+      {
+        value: 'whatsapp',
+        optionText: 'WhatsApp',
+      },
+    ],
+    selectText: 'Connection',
+    checkbox: {
+      url: 'https://example.com/policy',
+      text: 'Privacy policy',
+    },
+    buttonText: 'Submit',
+  },
+};
