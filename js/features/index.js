849e20893c0334fc48601c0e30fd893609ca3617
Nina Konopleva <orangemise@gmail.com>
2024-09-16 15:39:50 +0300

    Refactored App

--- js/features/index.js
+++ js/features/index.js
@@ -4,6 +4,5 @@ export { Input } from './Input/index.js';
 export { Lang } from './Lang/index.js';
 export { Logo } from './Logo/index.js';
 export { Navigation } from './Navigation/index.js';
-export { Developer } from './Developer/index.js';
 export { Select } from './Select/index.js';
 export { Theme } from './Theme/index.js';
