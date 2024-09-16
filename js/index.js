a61825ac72cf0f912418ff7b32fb613b4d43f4f0
Nina Konopleva <orangemise@gmail.com>
2024-09-16 21:47:50 +0300

    Refactored index file

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 import { App } from './app/App.js';
 import { addHandlers } from './addHandlers.js';
+import { API_URL } from './config.js';
 
-export const DATA_URL = 'https://ng-free-zen-knop-default-rtdb.firebaseio.com/.json';
 const $root = document.querySelector('#root');
 
-fetch(DATA_URL)
-  .then((responce) => responce.json())
-  .then((responceData) => {
-    if ($root) $root.innerHTML = App(responceData.en);
-    addHandlers(responceData.en);
+fetch(API_URL)
+  .then((response) => response.json())
+  .then((responseData) => {
+    if ($root) $root.innerHTML = App(responseData.en);
+    addHandlers(responseData.en);
   })
-  .catch((error) => console.error('Не удалось получить данные:', error));
+  .catch((error) => console.error(error));
