621c1f6acb9df5cbaa3a8b8cb566c84e9d5eba41
Nina Konopleva <orangemise@gmail.com>
2024-09-24 17:41:55 +0300

    Add Preloader

---
+++ js/ui/Preloader/Preloader.js
@@ -0,0 +1,20 @@
+/**
+ * @function Preloader
+ * @returns {string}
+ */
+
+export const Preloader = () => `
+  <div class="preloader-area">
+    <div class="preloader">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+`;
